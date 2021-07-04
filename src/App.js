97da43421de092231b6f2e2ca36057b137ce16fd
Erika Espejo <erikaespejo95@gmail.com>
2021-07-04 15:51:44 -0500

    welcome component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Welcome from './Welcome';
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      names: ["Juan", "Pedro", "Germán"]
-    }
+      names: ['Juan', 'Pedro', 'Germán'],
+    };
   }
 
   render() {
     return (
       <div>
-        {this.state.names.map((name) =>
-          <Welcome name={name} />
-        )}
+        {this.state.names.map((name, index) => (
+          <Welcome name={name} key={index} />
+        ))}
       </div>
     );
   }
---
+++ src/Welcome.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Welcome({ name }) {
+  return <h1>Hola {name}</h1>;
+}
